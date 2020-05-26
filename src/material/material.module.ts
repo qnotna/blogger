e31cf580debb9ce711155fcc8a74d471ab007606
Nam Nguyen <nam.nguyen@allmyhomes.com>
2020-05-26 22:59:25 +0200

    [ADD] Material Setup & use in auth/login component, updated RADME.md

--- src/material/material.module.ts
+++ src/material/material.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -12,7 +11,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
-    CommonModule,
     MatInputModule,
     MatCardModule,
     MatButtonModule,
