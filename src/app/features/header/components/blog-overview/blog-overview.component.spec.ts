81b937908b56a1f70db226739158283433e8d480
Nam Nguyen <nam.nguyen@allmyhomes.com>
2020-05-29 02:14:33 +0200

    [ADD] adjusted unit-tests, setup test w/ jest

--- src/app/features/header/components/blog-overview/blog-overview.component.spec.ts
+++ src/app/features/header/components/blog-overview/blog-overview.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BlogOverviewComponent } from './blog-overview.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('BlogOverviewComponent', () => {
   let component: BlogOverviewComponent;
@@ -8,7 +9,8 @@ describe('BlogOverviewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BlogOverviewComponent ]
+      declarations: [ BlogOverviewComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
