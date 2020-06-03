f24477a3cf16b30bb1fe9601ecf795bdafe6f67d
Till <s75453@beuth-hochschule.de>
2020-06-03 14:30:37 +0200

    test 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
 
 // Modules
 import { GoogleApiModule, NG_GAPI_CONFIG } from 'ng-gapi';
@@ -17,6 +18,7 @@ import { MainComponent } from './features/main/container/main.component';
 import { LoginComponent } from './features/login/login.component';
 import { BlogOverviewComponent } from './features/header/components/blog-overview/blog-overview.component';
 import { HeaderComponent } from './features/header/container/header.component';
+import { MyDialogComponent } from './features/my-dialog/my-dialog.component';
 
 
 @NgModule({
@@ -25,7 +27,8 @@ import { HeaderComponent } from './features/header/container/header.component';
     MainComponent,
     LoginComponent,
     BlogOverviewComponent,
-    HeaderComponent
+    HeaderComponent,
+    MyDialogComponent,
   ],
   imports: [
     BrowserModule,
@@ -36,7 +39,12 @@ import { HeaderComponent } from './features/header/container/header.component';
     FormsModule,
     GoogleApiModule.forRoot({
       provide: NG_GAPI_CONFIG, useValue: gapiClientConfig
-    })
+    }),
+    FormsModule,
+    MatDialogModule,
+  ],
+  entryComponents:[
+    MyDialogComponent
   ],
   bootstrap: [AppComponent]
 })
