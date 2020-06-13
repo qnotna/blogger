62b07db036febc3a8a1befdda76de071bb463ce5
Nam Nguyen <nam.nguyen@allmyhomes.com>
2020-06-13 19:47:00 +0200

    [ADD] loading spinner for post-detailview

--- src/app/features/post-overview/components/post-detailview/post-detailview.component.ts
+++ src/app/features/post-overview/components/post-detailview/post-detailview.component.ts
@@ -1,22 +1,29 @@
-import { Component, OnInit } from "@angular/core";
-import { Post } from "../../../../models/posts.model";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Post } from '../../../../models/posts.model';
 import { ActivatedRoute, Params } from '@angular/router';
 import { PostOverviewService } from '../../services/post-overview.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription, BehaviorSubject } from 'rxjs';
 
 @Component({
-  selector: "app-post-detailview",
-  templateUrl: "./post-detailview.component.html",
-  styleUrls: ["./post-detailview.component.scss"],
+  selector: 'app-post-detailview',
+  templateUrl: './post-detailview.component.html',
+  styleUrls: ['./post-detailview.component.scss'],
 })
-export class PostDetailviewComponent implements OnInit {
+export class PostDetailviewComponent implements OnInit, OnDestroy {
   post$: Observable<Post>;
-  
+  routeSub: Subscription;
+  isLoading$: BehaviorSubject<boolean>;
+
   constructor(private currentRoute: ActivatedRoute, private service: PostOverviewService) {}
 
   ngOnInit(): void {
-    this.currentRoute.params.subscribe((params: Params) => {
+    this.isLoading$ = this.service.isLoading$;
+    this.routeSub = this.currentRoute.params.subscribe((params: Params) => {
       this.post$ = this.service.getPostById(params.postId, params.blogId);
     });
+  }
+
+  ngOnDestroy(): void {
+    this.routeSub.unsubscribe();
   }
 }
