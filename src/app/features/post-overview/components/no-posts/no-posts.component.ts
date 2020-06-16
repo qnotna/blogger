e3150629ecdf366f79d6ec2162bd8b5d6ca00979
Nam Nguyen <nam.nguyen@allmyhomes.com>
2020-06-16 15:30:03 +0200

    [CHG] moved no-posts to post-overview components folder, changed retrieving and displaying of errors regarding property binding

---
+++ src/app/features/post-overview/components/no-posts/no-posts.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Output, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { renderNoPostsConfig, NoPostsConfig } from './no-posts.config';
+
+@Component({
+  selector: 'app-no-posts',
+  templateUrl: './no-posts.component.html',
+  styleUrls: ['./no-posts.component.scss']
+})
+export class NoPostsComponent implements OnChanges {
+  @Input() noContent: boolean;
+  @Input() noResults: boolean;
+  @Output() notifyParent = new EventEmitter<void>();
+  config: NoPostsConfig;
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.config = renderNoPostsConfig(this.noContent, this.noResults);
+  }
+}
