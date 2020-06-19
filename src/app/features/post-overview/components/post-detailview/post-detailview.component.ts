be15e85eeaac1286ca7a52a73db5ba1c3358947f
steffyMacht <st.phunket@gmail.com>
2020-06-19 10:11:16 +0200

    [ADD] add delete function in post-detailview

--- src/app/features/post-overview/components/post-detailview/post-detailview.component.ts
+++ src/app/features/post-overview/components/post-detailview/post-detailview.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Post } from '../../../../models/posts.model';
-import { ActivatedRoute, Params } from '@angular/router';
-import { PostOverviewService } from '../../services/post-overview.service';
-import { Observable, Subscription, BehaviorSubject } from 'rxjs';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  Output,
+  EventEmitter,
+  Input,
+} from "@angular/core";
+import { Post } from "../../../../models/posts.model";
+import { ActivatedRoute, Params } from "@angular/router";
+import { PostOverviewService } from "../../services/post-overview.service";
+import { Observable, Subscription, BehaviorSubject } from "rxjs";
+/* import { EventEmitter } from 'protractor'; */
+import { DeleteRequestBody } from "src/app/models/post-request-body.model";
 
 @Component({
-  selector: 'app-post-detailview',
-  templateUrl: './post-detailview.component.html',
-  styleUrls: ['./post-detailview.component.scss'],
+  selector: "app-post-detailview",
+  templateUrl: "./post-detailview.component.html",
+  styleUrls: ["./post-detailview.component.scss"],
 })
 export class PostDetailviewComponent implements OnInit, OnDestroy {
   post$: Observable<Post>;
   routeSub: Subscription;
   isLoading$: BehaviorSubject<boolean>;
 
-  constructor(private currentRoute: ActivatedRoute, private service: PostOverviewService) {}
+  constructor(
+    private currentRoute: ActivatedRoute,
+    private service: PostOverviewService
+  ) {}
 
   ngOnInit(): void {
     this.isLoading$ = this.service.isLoading$;
@@ -25,5 +37,15 @@ export class PostDetailviewComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.routeSub.unsubscribe();
+  }
+
+  onDelete(): void {
+    this.routeSub = this.currentRoute.params.subscribe((params: Params) => {
+      this.service
+        .removePostFrom(params.blogId, params.postId)
+        .subscribe((_) =>
+          this.service.navigateTo(`/home/blogs/${params.blogId}`)
+        );
+    });
   }
 }
